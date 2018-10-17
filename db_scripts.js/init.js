eebd109c1328401a210475b7ffac260276bf94fc
Ivan-PC\Ivan <ivan.ivanov.7c6@gmail.com>
2018-10-17 14:13:14 +0500

    Add mock data for template, drafts and certificates. Not tested yet

--- db_scripts.js/init.js
+++ db_scripts.js/init.js
@@ -1,12 +1,16 @@
 const Db = require('tingodb')().Db;
 
-const db = new Db('../db', {});
+const db = new Db('db', {});
 
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, doloremque explicabo nemo voluptate facilis assumenda recusandae? Temporibus nesciunt, porro voluptatem eaque a similique aliquid, ducimus commodi, exercitationem quasi vel deserunt unde mollitia amet illum rem id fugiat dicta saepe vitae magni repellendus quas veniam eligendi? Voluptatem eos repudiandae numquam neque. Voluptatum nulla reiciendis praesentium dolorem vitae voluptate tempora at fuga, esse, mollitia asperiores sequi voluptates modi rem rerum aspernatur commodi suscipit atque iure earum architecto totam natus laborum! Asperiores iure voluptates similique expedita, totam optio placeat fugiat eum. Atque tempore dignissimos voluptatem placeat dicta consequuntur soluta tempora distinctio laborum sint neque rerum vel, assumenda dolorem aliquam id totam aliquid sed blanditiis eligendi officiis in! Deserunt aut molestias, commodi ducimus, cupiditate rerum ullam consequatur, ipsa laborum laboriosam explicabo possimus nulla consectetur mollitia eos qui sequi illo cumque iste id quos natus expedita optio. Nobis illo nemo eligendi! Ducimus, adipisci! Vel, hic!';
 
 // addMockCertificates(db, lorem)
 
-generateRandomEthAccount()
+// generateRandomEthAccount()
+
+addMockIssuers(db)
+	.then(addMockTemplates)
+// generateNames()
 
 function addMockIssuers(db) {
 	let issuers = [];
@@ -39,7 +43,11 @@ function addMockIssuers(db) {
 		})
 	})
 
-	insertMockToCollection(db, 'issuers', issuers)
+	// console.log(issuers)
+
+	issuers = removeDuplicates(issuers, (obj, o) => obj.email == o.email)
+	// console.log(issuers)
+	return insertMockToCollection(db, 'issuers', issuers)
 }
 
 function generateRandomEthAccount() {
@@ -70,7 +78,7 @@ function generateNames() {
 
 	const first_names = ['John', 'Ivan', 'Albert'];
 	const surnames = ['Doe', 'Ivanov', 'Shtein']
-	const company_names = ['SONY Ltd.', 'Panasonyc', 'Valve']
+	const company_names = ['SONY', 'Panasonyc', 'Valve']
 
 	let names = []
 
@@ -82,16 +90,17 @@ function generateNames() {
 			})
 		})
 	})
-
+	// console.log(names)
 	let names_companies = []
 
 	names.forEach(name => {
 		company_names.forEach(c_name => {
+			// console.log('name', name)
 			let rnd = Math.random();
 			if (rnd <= 0.3) {
 				names_companies.push({
-					name: name.f_name,
-					surname: name.s_name,
+					name: name.name,
+					surname: name.surname,
 					company_name: c_name
 				})
 			} else if (rnd > 0.3 && rnd <= 0.6) {
@@ -102,19 +111,180 @@ function generateNames() {
 				})
 			} else if (rnd > 0.6) {
 				names_companies.push({
-					name: name.f_name,
-					surname: name.s_name,
+					name: name.name,
+					surname: name.surname,
 					company_name: null
 				})
 			}
 
 		})
 	})
-
+	// console.log('names_companies', names_companies)
 	return names_companies;
 }
 
+// INPUTS
+const TEXT_INPUT = 'TEXT_INPUT';
+const NUMBER_INPUT = 'NUMBER_INPUT';
+const CHECKBOX_INPUT = 'CHECKBOX_INPUT';
+const RADIOBUTTON_INPUT = 'RADIOBUTTON_INPUT';
+const SWITCH_INPUT = 'SWITCH_INPUT';
+const HEXNUMB_INPUT = 'HEXNUMB_INPUT';
+const SELECT = 'SELECT';
+const SLIDER = 'SLIDER';
+const TEXTAREA = 'TEXTAREA';
+
+function addMockTemplates(db) {
+	let templates = []
+
+	let standard_tmpl = {
+		name: 'Standard Template 1',
+		fields: [
+			{
+				type: TEXT_INPUT,
+				caption: 'Name',
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Issuer Name',
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Issuer Surname',
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Issuer Company Name',
+			},
+			{
+				type: HEXNUMB_INPUT,
+				caption: 'Issuer Eth Account'
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Receiver Name',
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Receiver Surname',
+			},
+			{
+				type: TEXT_INPUT,
+				caption: 'Receiver Company Name',
+			},
+			{
+				type: HEXNUMB_INPUT,
+				caption: 'Receiver Eth Account'
+			},
+			{
+				type: TEXTAREA,
+				caption: 'Description'
+			}
+		]
+	}
+
+	templates.push(standard_tmpl)
+
+	return insertMockToCollection(db, 'templates', templates)
+}
+
+const certificateNames = ['Java EE', 'Java SE', 'ECMAScript 5', 'ECMAScript 7', 'CSS 3']
+
+function addMockDrafts(db, lorem) {
+
+	return new Promise(function (resolve, reject) {
+		db.collection('issuers').find({}, function (err, issuers) {
+			console.log(issuers)
+
+			if (!err) {
+				db.collection('templates').findOne({}, function (err, tmpl) {
+					if (err)
+						reject(err)
+					else {
+						let mockDrafts = generateMockDrafts(issuers, lorem, tmpl);
+						resolve(insertMockToCollection(db, 'drafts', mockDrafts))
+					}
+
+				})
+			} else {
+				reject(err)
+			}
+		})
+	})
+
+	function generateMockDrafts(issuers, lorem, tmpl) {
+		let drafts = [];
+
+		for (let i = 0; i < 5; i++) {
+			let name = getRndArrItem(certificateNames)
+			drafts.push({
+				template: tmpl._id,
+				name: name,
+				issuer: getRndArrItem(issuers)._id,
+				description: name + ' ' + name + ' ' + lorem,
+				timestamp: Date.now(),
+			})
+
+		}
+
+		return drafts;
+	}
+
+}
+
+const receiverNames = ['Vasya', 'Petya', 'Ivan', 'John', 'Alice', 'Nick', 'Maria'];
+const receiverSurnames = ['Petrov', 'Smirnov', 'Ivanov', 'Doe', 'Stein', 'Malovich', 'Magdolen'];
+const receiverCompNames = ['Sany', 'Morgan', 'Papasonyc', 'Minihard', 'Gazoil']
+
 function addMockCertificates(db, lorem) {
+
+	return new Promise(function (resolve, reject) {
+		db.collection('drafts').find({}, function (err, drafts) {
+
+			if (!err) {
+				db.collection('templates').findOne({}, function (err, tmpl) {
+					if (err)
+						reject(err)
+					else {
+						let mockCertificates = generateMockCertificates(drafts, lorem, tmpl);
+						resolve(insertMockToCollection(db, 'drafts', mockCertificates))
+					}
+
+				})
+			} else {
+				reject(err)
+			}
+		})
+	})
+
+	function generateMockCertificates(drafts, lorem, tmpl) {
+		let certificates = [];
+
+		for (let i = 0; i < 50; i++) {
+			let draft = getRndArrItem(drafts)
+			certificates.push({
+				template: draft.template,
+				name: draft.name,
+				issuer: draft.issuer,
+				description: draft.description,
+				receiver: {
+					name: getRndArrItem(receiverNames),
+					surname: getRndArrItem(receiverSurnames),
+					company_name: Math.random() > 0.5 ? getRndArrItem(receiverCompNames) : null,
+					eth_account: generateRandomEthAccount()
+				},
+				status: 'unissued',
+				timestamp: Date.now(),
+			})
+
+		}
+
+		return certificates;
+	}
+
+}
+
+function addMockCertificates_Old(db, lorem) {
 
 	let certificates = [];
 
@@ -136,9 +306,36 @@ function addMockCertificates(db, lorem) {
 }
 
 function insertMockToCollection(db, collection_name, mock_arr) {
-	db.collection(collection_name)
-		.insert(mock_arr, function (err, result) {
-			console.log(collection_name, result.lenght, err)
 
-		})
+	return new Promise(function (resolve, reject) {
+		db.collection(collection_name)
+			.insert(mock_arr, function (err, result) {
+				console.log(collection_name, result, err)
+				if (!err)
+					resolve(db)
+				else
+					reject(err)
+			})
+	})
+
+}
+
+function removeDuplicates(arr, condition) {
+	return arr.reduce((unique, o) => {
+		if (!unique.some(obj => condition(obj, o))) {
+			unique.push(o);
+		}
+		return unique;
+	}, []);
+}
+
+function getRndArrItem(arr) {
+	let len = arr.length;
+
+	let rndPos = Math.floor(Math.random() * len);
+	if (rndPos == len)
+		rndPos = len - 1;
+
+	return arr[rndPos]
+
 }
